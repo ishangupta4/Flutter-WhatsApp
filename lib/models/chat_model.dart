612701b3b2992fb271f606e9ba8f3ccebcf0de21
ishan <ishangupta004@gmail.com>
2020-06-18 12:20:23 +0530

    chats created

---
+++ lib/models/chat_model.dart
@@ -0,0 +1,67 @@
+class ChatModel {
+  final String name;
+  final String message;
+  final String time;
+  final String avatarUrl;
+
+  ChatModel({this.name, this.message, this.time, this.avatarUrl});
+}
+
+List<ChatModel> dummyData = [
+  new ChatModel(
+    name: "Ishan Gupta",
+    message: "Hey everyone",
+    time: "11:47",
+    avatarUrl:
+        "https://cdn.mos.cms.futurecdn.net/Hai4EsJgSKgZoFw69VN6NY-650-80.jpg.webp",
+  ),
+  new ChatModel(
+    name: "Rishabh Sharma",
+    message: "Hey there, it's me",
+    time: "11:49",
+    avatarUrl:
+        "https://cdn.britannica.com/49/182849-050-4C7FE34F/scene-Iron-Man.jpg",
+  ),
+  new ChatModel(
+    name: "Ishan Gupta",
+    message: "Hey everyone",
+    time: "11:47",
+    avatarUrl:
+        "https://cdn.mos.cms.futurecdn.net/Hai4EsJgSKgZoFw69VN6NY-650-80.jpg.webp",
+  ),
+  new ChatModel(
+    name: "Rishabh Sharma",
+    message: "Hey there, it's me",
+    time: "11:49",
+    avatarUrl:
+        "https://cdn.britannica.com/49/182849-050-4C7FE34F/scene-Iron-Man.jpg",
+  ),
+  new ChatModel(
+    name: "Ishan Gupta",
+    message: "Hey everyone",
+    time: "11:47",
+    avatarUrl:
+        "https://cdn.mos.cms.futurecdn.net/Hai4EsJgSKgZoFw69VN6NY-650-80.jpg.webp",
+  ),
+  new ChatModel(
+    name: "Rishabh Sharma",
+    message: "Hey there, it's me",
+    time: "11:49",
+    avatarUrl:
+        "https://cdn.britannica.com/49/182849-050-4C7FE34F/scene-Iron-Man.jpg",
+  ),
+  new ChatModel(
+    name: "Ishan Gupta",
+    message: "Hey everyone",
+    time: "11:47",
+    avatarUrl:
+        "https://cdn.mos.cms.futurecdn.net/Hai4EsJgSKgZoFw69VN6NY-650-80.jpg.webp",
+  ),
+  new ChatModel(
+    name: "Rishabh Sharma",
+    message: "Hey there, it's me",
+    time: "11:49",
+    avatarUrl:
+        "https://cdn.britannica.com/49/182849-050-4C7FE34F/scene-Iron-Man.jpg",
+  ),
+];
---
+++ lib/whatsapp_home.dart
@@ -0,0 +1,72 @@
+import 'package:flutter/material.dart';
+import 'package:whatsapp_clone/pages/call_screen.dart';
+import 'package:whatsapp_clone/pages/camera_screen.dart';
+import 'package:whatsapp_clone/pages/chat_screeen.dart';
+import 'package:whatsapp_clone/pages/status_screen.dart';
+
+class WhatsAppHome extends StatefulWidget {
+  @override
+  _WhatsAppHomeState createState() => _WhatsAppHomeState();
+}
+
+class _WhatsAppHomeState extends State<WhatsAppHome>
+    with SingleTickerProviderStateMixin {
+  TabController _tabController;
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    _tabController = new TabController(length: 4, vsync: this, initialIndex: 1);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+      appBar: new AppBar(
+        title: new Text("WhatsApp"),
+        elevation: 0.7,
+        bottom: new TabBar(
+          controller: _tabController,
+          indicatorColor: Colors.white,
+          tabs: <Widget>[
+            new Tab(
+              icon: new Icon(Icons.camera_alt),
+            ),
+            new Tab(
+              text: "Chats",
+            ),
+            new Tab(
+              text: "Status",
+            ),
+            new Tab(
+              text: "calls",
+            ),
+          ],
+        ),
+        actions: <Widget>[
+          new Icon(Icons.search),
+          new Padding(padding: const EdgeInsets.symmetric(horizontal: 5.0)),
+          new Icon(Icons.more_vert),
+        ],
+      ),
+      body: new TabBarView(
+        controller: _tabController,
+        children: <Widget>[
+          new CameraScreen(),
+          new ChatScreen(),
+          new StatusScreen(),
+          new CallScreen(),
+        ],
+      ),
+      floatingActionButton: new FloatingActionButton(
+        onPressed: () => print("open chats"),
+        backgroundColor: Theme.of(context).accentColor,
+        child: new Icon(
+          Icons.message,
+          color: Colors.white,
+        ),
+      ),
+    );
+  }
+}
